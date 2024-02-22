c02f8e1b4e792325917006ae0ce584a2a6c96c42
shersha123 <ukkasnaina99@gmail.com>
2024-02-22 17:01:09 +0530

    hi

--- models.js
+++ models.js
@@ -3,11 +3,11 @@ import mongoose from 'mongoose';
 
 const deliveryAgentSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    mobileNumber: { type: String, required: true },
+    mobileNumber: { type: Number, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
     address: { type: String, required: true },
-    pincode: { type: String, required: true },
+    pincode: { type: Number, required: true },
     city: { type: String, required: true }
 });
 
